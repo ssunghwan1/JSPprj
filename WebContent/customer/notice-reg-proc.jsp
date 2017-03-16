3648e7a7a607f079517dd7142f3bdd631dcc36b5
이세은 <thjds8@gmail.com>
2017-03-16 12:11:57 +0900

    newlec : 게시글 등록 추가

--- WebContent/customer/notice-reg-proc.jsp
+++ WebContent/customer/notice-reg-proc.jsp
@@ -1,8 +1,19 @@
+<%@page import="com.newlecture.web.dao.mysql.MySQLNoticeDao"%>
+<%@page import="com.newlecture.web.data.dao.NoticeDao"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 
 <% 
 	String title = request.getParameter("title");
 	String content = request.getParameter("content");
+	
+	NoticeDao noticeDao = new MySQLNoticeDao();
+	int result = noticeDao.add(title, content, "SAKURA");
+	
+	if(result > 0)
+		// 목록으로 가기 (입력도구, 출력도구, 저장소)
+		response.sendRedirect("notice.jsp");
+	//else
+		// 에러 페이지로 가기1
 %>    
     
--- src/com/newlecture/web/dao/mysql/MySQLNoticeDao.java
+++ src/com/newlecture/web/dao/mysql/MySQLNoticeDao.java
@@ -111,53 +111,53 @@ public class MySQLNoticeDao implements NoticeDao{
 
 	@Override
 	public int add(Notice notice) {
-//		String sql = "INSERT INTO NOTICE VALUES(?, ?, ?, ?, NOW(), ?);"; //데이터에만 ? 사용 가능하기 때문에, title은 "+field+"라고 씀
-//		List<NoticeView> list = new ArrayList<>();
-//		
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver");
-//			
-//			String url = "jdbc:mysql://211.238.142.84/newlecture?autoReconnect=true&amp;useSSL=false&characterEncoding=UTF-8"; // DB연결
-//			Connection con = DriverManager.getConnection(url, "newlec", "sclass"); // 드라이브 로드
-//			PreparedStatement st = con.prepareStatement(sql);
-//			st.setString(1, '%'+query+'%');
-//			
-//			ResultSet rs = st.executeQuery();
-//			
-//			NoticeView nv = null;
-//
-//			while (rs.next()) {
-//				nv = new NoticeView();
-//				nv.setCode(rs.getString("CODE"));
-//				nv.setTitle(rs.getString("TITLE"));
-//				nv.setContent(rs.getString("CONTENT"));
-//				nv.setWriter(rs.getString("WRITER"));
-//				nv.setRegDate(rs.getDate("REGDATE"));
-//				nv.setHit(rs.getInt("HIT"));
-//				//NoticeView 컬럼
-//				nv.setWriterName(rs.getString("WRITER_NAME"));
-//				nv.setCommentCount(rs.getInt("COMMENT_COUNT"));
-//				
-//				list.add(nv);
-//			}
-//
-//			rs.close();
-//			st.close();
-//			con.close();
-//			
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		
-		return 0;
+		String codeSql = "SELECT MAX(CAST(CODE AS UNSIGNED))+1 CODE FROM NOTICE";
+		
+		String sql = "INSERT INTO NOTICE(CODE,TITLE,WRITER,CONTENT) VALUES(?,?,?,?)"; 
+		int result = 0;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String url = "jdbc:mysql://211.238.142.84/newlecture?autoReconnect=true&amp;useSSL=false&characterEncoding=UTF-8"; // DB연결
+			Connection con = DriverManager.getConnection(url, "newlec", "sclass"); // 드라이브 로드
+			
+			Statement codeSt = con.createStatement();
+			ResultSet rs = codeSt.executeQuery(codeSql);
+			rs.next();
+			String code = rs.getString("CODE");
+			rs.close();
+			codeSt.close();
+			
+			PreparedStatement st = con.prepareStatement(sql);
+			
+			st.setString(1, code);
+			st.setString(2, notice.getTitle());
+			st.setString(3, notice.getWriter());
+			st.setString(4, notice.getContent());
+			
+			result = st.executeUpdate();
+
+			st.close();
+			con.close();
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 
 	@Override
 	public int add(String title, String content, String writer) {
-		// TODO Auto-generated method stub
-		return 0;
+		Notice notice = new Notice();
+		notice.setTitle(title);
+		notice.setWriter(writer);
+		notice.setContent(content);
+		
+		return add(notice);
 	}
 
 }
